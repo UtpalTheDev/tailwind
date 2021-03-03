744723bfac23c7f8b61d3a2ed2b753eaf7e081e1
UtpalTheDev <utpalpati8@gmail.com>
2021-03-03 17:10:32 +0530

    initial commit
    react tailwind template

---
+++ tailwind-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div class="w-64 h-full bg-green-500">hello</div>
+    </div>
+  );
+}
+
+export default App;
